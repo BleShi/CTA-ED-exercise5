7a2cddfb2c2ad0b5d092ecd7bb6fd0649491c058
Marion Lieutaud <72838935+MarionLieutaud@users.noreply.github.com>
2024-04-11 16:09:29 +0100

    Create Feedback.R

---
+++ Feedback.R
@@ -0,0 +1,26 @@
+# Feedback: CTA exercise 5 (2024)
+
+# question 1
+# -----------------------------------------------------------
+# good work
+
+# question 2
+# -----------------------------------------------------------
+# very good. Very well commented code. 
+# you should spend more time and writing space on explanation of decision-making (eg number of topics)
+# and on interpretation (e.g. you didn't explain the consistency analysis)
+
+# question 3
+# -----------------------------------------------------------
+# the code is good but you didn't interpret the output
+# would have been worth checking out the PreText documentation to figure out how to read the output
+# and what to do with it
+
+# overall code quality: 
+# -----------------------------------------------------------
+# very good, good commenting etc
+
+# overall interpretation: 
+# -----------------------------------------------------------
+# you need to invest more still in writing out interpretation of results. Any plot/output should be accompanied by written
+# text that reports the key results and interpret them for the research question.
